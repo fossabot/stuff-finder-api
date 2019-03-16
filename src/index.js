7ef416db3a70407da522f0423e4fd37a194dd7bc
Romain Racamier-Lafon <romain.racamier@gmail.com>
2019-03-16 18:42:45 +0100

    simple http server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hey hey !')
+const http = require('http')
+const port = 1664
+
+http.createServer(function (req, res) {
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify({ hello: 'world' }))
+}).listen(port, function () {
+  console.log(`server start at port ${port}`)
+})
